648e57e8d18468c28662db3fd47e786dd8f4eebf
Shankar <psgganesh@gmail.com>
2023-03-21 12:56:13 +1100

    Enabling support for cdk-nag at the cdk stack level

--- bin/coffee-listing-app.ts
+++ bin/coffee-listing-app.ts
@@ -2,8 +2,14 @@
 import "source-map-support/register";
 import * as cdk from "aws-cdk-lib";
 import { CoffeeListingAppStack } from "../lib/coffee-listing-app-stack";
+import { AwsSolutionsChecks } from 'cdk-nag'
+import { Aspects } from 'aws-cdk-lib';
 
 const app = new cdk.App();
+
+// Add the cdk-nag AwsSolutions Pack with extra verbose logging enabled.
+Aspects.of(app).add(new AwsSolutionsChecks({ verbose: true }))
+
 new CoffeeListingAppStack(app, "CoffeeListingAppStack", {
   stackName: "CoffeeListingAppStack",
 });
